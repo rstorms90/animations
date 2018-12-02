d6f9eb32a279ad1bf58bdf2a9de8f3ec87b7ee8d
Russ Storms <rstorms90@gmail.com>
2018-12-01 20:21:04 -0700

    one step at a time🕰

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-TweenMax.staggerFrom('.box', 0.5, {opacity:0, y:200, rotation:360, scale:2, delay:0.5}, 0.2)
-TweenMax.to('.box', 0.5, {rotation:360, delay:2})
+TweenMax.staggerFrom('.box', 0.5, {opacity:0, x:200, rotation:360, scale:2, delay:0.5}, 0.2)
+TweenMax.staggerTo('.box', 0.5, {opacity:0, x:200, rotation:360, scale:2, delay:3}, 0.2)
