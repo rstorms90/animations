a0ba08dae8b63e6cc1b131d9c1f83f9199385f6e
Russ Storms <rstorms90@gmail.com>
2018-12-02 11:33:55 -0700

    shiny name ✨

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    TweenMax.staggerFrom('.chizette', 3, {opacity:0, x:500, scale:0.2, delay: 0.5}, 0.2)
-    const rainbowName = () => {
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#fb9a08", scale:2, delay:1}, 0.1)
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#fffb13", scale:2, delay:2}, 0.1)
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#23d81d", scale:2, delay:3}, 0.1)
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#04d1d8", scale:2, delay:4}, 0.1)
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#062af5", scale:2, delay:5}, 0.1)
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#bd06f5", scale:2, delay:6}, 0.1)
-        TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#f50606", scale:2, delay:7}, 0.1) 
+    TweenMax.staggerFrom('.chizette', 2, {opacity:0, delay: 0.1}, 0.1)
+    const shinyName = () => {
+        TweenMax.staggerTo('.chizette', 0.1, {opacity:0, color:"#white", delay:1}, 0.05)
+        TweenMax.staggerTo('.chizette', 0.1, {opacity:1, color:"#0051fff", delay:1.1}, 0.06)
     }
-    rainbowName()
+    shinyName()
     window.setInterval(function(){
-        rainbowName()
-    }, 7200)
+        shinyName()
+    }, 1600)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////RAINBOW NAME\\\\\\
+// document.addEventListener("DOMContentLoaded", () => {
+//     TweenMax.staggerFrom('.chizette', 3, {opacity:0, x:500, scale:0.2, delay: 0.5}, 0.2)
+//     const rainbowName = () => {
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#fb9a08", delay:1}, 0.1)
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#fffb13", delay:2}, 0.1)
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#23d81d", delay:3}, 0.1)
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#04d1d8", delay:4}, 0.1)
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#062af5", delay:5}, 0.1)
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#bd06f5", delay:6}, 0.1)
+//         TweenMax.staggerTo('.chizette', 0.2, {opacity:1, color:"#f50606", delay:7}, 0.1) 
+//     }
+//     rainbowName()
+//     window.setInterval(function(){
+//         rainbowName()
+//     }, 7200)
+// })
